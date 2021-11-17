f53647ce56b5f7d0177421d986ecbdc0b932b77d
securepeacock <92804416+securepeacock@users.noreply.github.com>
2021-11-17 09:44:49 -0500

    Delete CompoundActions/T1547.001 - Registry Run Keys directory

--- CompoundActions/T1547.001 - Registry Run Keys/VFS/AtomicTest7.ps1
+++
@@ -1,6 +0,0 @@
-$Target = "C:\Windows\System32\calc.exe"
-$ShortcutLocation = "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\calc_exe.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Create = $WScriptShell.CreateShortcut($ShortcutLocation)
-$Create.TargetPath = $Target
-$Create.Save()
